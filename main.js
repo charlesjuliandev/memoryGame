7e5ab8898d3f753595cc18827e526a07d3dca56f
Charles Julian <cjulian@bermangrp.com>
2017-09-08 00:30:00 -0400

    added overlay for bio and click events and minimal styling

--- main.js
+++ main.js
@@ -1,53 +1,81 @@
-var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
-var memory_array = [
+// var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+var memory_array_men = [
 	{
 		"title": "James Baldwin",
-		"image": "./images/jamesbaldwin.jpg"
+		"image": "./images/jamesbaldwin.jpg",
+		"bio": "James Baldwin was an American writer and social critic. His essays, as collected in Notes of a Native Son, explore palpable yet unspoken intricacies of racial, sexual, and class distinctions in Western societies, most notably in mid-20th-century America.",
+		"link": "https://en.wikipedia.org/wiki/James_Baldwin"
 	},
 	{
 		"title": "James Baldwin",
-		"image": "./images/jamesbaldwin.jpg"
+		"image": "./images/jamesbaldwin.jpg",
+		"bio": "James Baldwin was an American writer and social critic. His essays, as collected in Notes of a Native Son, explore palpable yet unspoken intricacies of racial, sexual, and class distinctions in Western societies, most notably in mid-20th-century America.",
+		"link": "https://en.wikipedia.org/wiki/James_Baldwin"
 	},
 	{
 		"title": "Langston Hughes",
-		"image": "./images/langstonhughes.jpg"
+		"image": "./images/langstonhughes.jpg",
+		"bio": "Langston Hughes was an important figure in the Harlem Renaissance and made the African American experience the subject of his writings, which ranged from poetry and plays to novels and newspaper columns.",
+		"link": "https://en.wikipedia.org/wiki/Langston_Hughes"
 	},
 	{
 		"title": "Langston Hughes",
-		"image": "./images/langstonhughes.jpg"
+		"image": "./images/langstonhughes.jpg",
+		"bio": "Langston Hughes was an important figure in the Harlem Renaissance and made the African American experience the subject of his writings, which ranged from poetry and plays to novels and newspaper columns.",
+		"link": "https://en.wikipedia.org/wiki/Langston_Hughes"
 	},
 	{
 		"title": "Jack Johnson",
-		"image": "./images/jackjohnson.jpg"
+		"image": "./images/jackjohnson.jpg",
+		"bio": "John Johnson became the first African American world heavyweight boxing champion.",
+		"link": "https://en.wikipedia.org/wiki/Jack_Johnson_(boxer)"
 	},
 	{
 		"title": "Jack Johnson",
-		"image": "./images/jackjohnson.jpg"
+		"image": "./images/jackjohnson.jpg",
+		"bio": "John Johnson became the first African American world heavyweight boxing champion.",
+		"link": "https://en.wikipedia.org/wiki/Jack_Johnson_(boxer)"
 	},
 	{
 		"title": "W.E.B. Dubois",
-		"image": "./images/webdubois.jpg"
+		"image": "./images/webdubois.jpg",
+		"bio": "W. E. B. Du Bois was an American sociologist, historian, civil rights activist, Pan-Africanist, author, writer and editor.",
+		"link": "https://en.wikipedia.org/wiki/W._E._B._Du_Bois"
 	},
 	{
 		"title": "W.E.B. Dubois",
-		"image": "./images/webdubois.jpg"
+		"image": "./images/webdubois.jpg",
+		"bio": "W. E. B. Du Bois was an American sociologist, historian, civil rights activist, Pan-Africanist, author, writer and editor.",
+		"link": "https://en.wikipedia.org/wiki/W._E._B._Du_Bois"
 	},
 	{
-		"title": "Toussaint Loverture",
-		"image": "./images/toussaintlouverture.jpg"
+		"title": "Toussaint L\\'Ouverture",
+		"image": "./images/toussaintlouverture.jpg",
+		"bio": "François-Dominique Toussaint Louverture, also known as Toussaint L'Ouverture was the best-known leader of the Haitian Revolution.",
+		"link": "https://en.wikipedia.org/wiki/Toussaint_Louverture"
 	},
 	{
-		"title": "Toussaint Loverture",
-		"image": "./images/toussaintlouverture.jpg"
+		"title": "Toussaint L\\'Ouverture",
+		"image": "./images/toussaintlouverture.jpg",
+		"bio": "François-Dominique Toussaint Louverture, also known as Toussaint L'Ouverture was the best-known leader of the Haitian Revolution.",
+		"link": "https://en.wikipedia.org/wiki/Toussaint_Louverture"
 	},
 	{
 		"title": "Benjamin Banneker",
-		"image": "./images/benjaminbanneker.jpg"
+		"image": "./images/benjaminbanneker.jpg",
+		"bio": "Benjamin Banneker was an almanac author, surveyor, naturalist, and farmer.",
+		"link": "https://en.wikipedia.org/wiki/Benjamin_Banneker"
 	},
 	{
 		"title": "Benjamin Banneker",
-		"image": "./images/benjaminbanneker.jpg"
+		"image": "./images/benjaminbanneker.jpg",
+		"bio": "Benjamin Banneker was an almanac author, surveyor, naturalist, and farmer.",
+		"link": "https://en.wikipedia.org/wiki/Benjamin_Banneker"
 	},
+]
+
+var memory_array_men2 = [
+	
 	{
 		"title": "Marcus Garvey",
 		"image": "./images/marcusgarvey.jpg"
@@ -96,6 +124,8 @@ var memory_array = [
 		"title": "Carter G Woodson",
 		"image": "./images/cartergwoodson.jpg"
 	},
+]
+var memory_array_women = [
 	{
 		"title": "Madam CJ Walker",
 		"image": "./images/madamcjwalker.jpg"
@@ -143,7 +173,11 @@ var memory_array = [
 	{
 		"title": "Bessie Coleman",
 		"image": "./images/bessiecoleman.jpg"
-	},
+	}
+];
+
+var memory_array_women2 = [
+	
 	{
 		"title": "Kathleen Cleaver",
 		"image": "./images/kathleencleaver.jpg"
@@ -188,19 +222,26 @@ var memory_array = [
 		"title": "Ida B Wells",
 		"image": "./images/idabwells.jpg"
 	},
-	
-];
+	{
+		"title": "Ida B Wells",
+		"image": "./images/idabwells.jpg"
+	}
+]
+
+
 var memory_values = [];
 var memory_tile_ids = [];
 var tiles_flipped = 0;
 
+var memory_array;
+
 function newBoard() {
 	tiles_flipped = 0;
-  memory_array = _.shuffle(memory_array);
-
+  (typeof memory_array === "undefined") ? memory_array = _.shuffle(memory_array_men) : memory_array = _.shuffle(memory_array_women) ;
+  console.log(memory_array)
   var output = '';
   _.each(memory_array, function(memory_array_value, index) {
-    output += '<div id="tile_'+ index +'" data-img='+ memory_array_value.image + ' onclick="memoryFlipTile(this,\''+ memory_array_value.title +'\')"></div>';
+    output += '<div id="tile_'+ index +'" data-img='+ memory_array_value.image + '  data-link='+ memory_array_value.link + ' data-bio="'+ memory_array_value.bio +'"  onclick="memoryFlipTile(this,\''+ memory_array_value.title +'\')"></div>';
   });
 
 	document.getElementById('memory_board').innerHTML = output;
@@ -227,12 +268,34 @@ function isThereIsAMatch() {
   return memory_values[0] == memory_values[1];
 }
 
-function matchCards() {
+function matchCards(tile, value) {
   tiles_flipped += 2;
   // Clear both arrays
   memory_values = [];
   memory_tile_ids = [];
+  showBio(tile, value);
 }
+
+function showBio(tile, value){
+	setTimeout(function(){
+		document.querySelector('.bio').textContent = "";
+		document.querySelector('.bio_wrapper').classList.remove('displayNone');
+		document.querySelector('.bio').textContent = tile.getAttribute('data-bio');
+		document.querySelector('.learn_more').setAttribute('href', tile.getAttribute('data-link'))
+	}, 500);
+	
+
+	closeBio();
+}
+
+function closeBio(){
+	document.querySelector('.close').addEventListener('click', function(e){
+		document.querySelector('.bio_wrapper').classList.add('displayNone');
+	});
+
+}
+
+
 
 function isGameOver() {
   // Check to see if the whole board is cleared
@@ -242,6 +305,8 @@ function isGameOver() {
 function gameIsOver() {
   alert("Board cleared... generating new board");
   document.getElementById('memory_board').innerHTML = "";
+  console.log("done")
+
   newBoard();
 }
 
@@ -250,10 +315,15 @@ function cardsDoNotMatch() {
 }
 
 function flipCard(tile, value) {
+	
 	var image = tile.getAttribute('data-img')
+	var text_elem = document.createElement('span');
+	text_elem.textContent = value;
+
   // tile.style.background = '#FFF';
   tile.style.backgroundImage = "url('"+ image+"')"
-  tile.innerHTML = value;
+  // tile.innerHTML = value;
+  tile.appendChild(text_elem)
   
 
 }
@@ -273,6 +343,7 @@ function flipCardBack() {
 }
 
 function memoryFlipTile(tile, value, image) {
+
 	if (canFlipCard(tile)) {
 		flipCard(tile, value);
     if (areNoCardsFlipped()) {
@@ -280,33 +351,12 @@ function memoryFlipTile(tile, value, image) {
 		} else if(isOneCardFlipped()) {
       setCardAsFlipped(tile, value);
       if(isThereIsAMatch()) {
-        matchCards();
+        matchCards(tile, value);
         if (isGameOver()) {
           gameIsOver();
         }
       } else {
   			cardsDoNotMatch();
-      }
-    }
-  }
-}
-
-function memoryFlipTile2(tile, value) {
-  if (canFlipCard(tile)) {
-    console.log('e1');
-    flipCard(tile, value);
-    setCardAsFlipped(tile, value);
-    if (isOneCardFlipped()) {
-      console.log('e2');
-      if (isThereIsAMatch()) {
-        console.log('e3');
-        matchCards();
-        if (isGameOver()) {
-          console.log('e4');
-          gameIsOver();
-        }
-      } else {
-        cardsDoNotMatch();
       }
     }
   }
